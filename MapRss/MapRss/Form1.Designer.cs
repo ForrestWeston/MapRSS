214552292945f59b8b16570a6ab5d5f07c0d49ac
Kyle Lafferty <kyle.lafferty@wsu.edu>
2014-10-19 18:06:39 -0700

    Updating UI

--- MapRss/MapRss/Form1.Designer.cs
+++ MapRss/MapRss/Form1.Designer.cs
@@ -33,7 +33,6 @@
             this.Feed = new System.Windows.Forms.Button();
             this.Map = new System.Windows.Forms.Button();
             this.Topic = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -41,9 +40,19 @@
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutMapRSSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.treeViewBase = new System.Windows.Forms.TreeView();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.addFeed = new System.Windows.Forms.Button();
+            this.removeFeed = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelContent = new System.Windows.Forms.Panel();
+            this.panelMenu = new System.Windows.Forms.Panel();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -52,7 +61,9 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panelContent, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panelMenu, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -113,17 +124,6 @@
             this.Topic.UseVisualStyleBackColor = true;
             this.Topic.Click += new System.EventHandler(this.Topic_Click);
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox1.Location = new System.Drawing.Point(3, 63);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(215, 483);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "RSS Groups";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
-            // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -180,6 +180,97 @@
             this.aboutMapRSSToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
             this.aboutMapRSSToolStripMenuItem.Text = "About MapRSS";
             // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.treeViewBase, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel4, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 63);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(215, 483);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // treeViewBase
+            // 
+            this.treeViewBase.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeViewBase.Location = new System.Drawing.Point(3, 51);
+            this.treeViewBase.Name = "treeViewBase";
+            this.treeViewBase.Size = new System.Drawing.Size(209, 429);
+            this.treeViewBase.TabIndex = 0;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 3;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel4.Controls.Add(this.addFeed, 1, 0);
+            this.tableLayoutPanel4.Controls.Add(this.removeFeed, 2, 0);
+            this.tableLayoutPanel4.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(209, 42);
+            this.tableLayoutPanel4.TabIndex = 1;
+            // 
+            // addFeed
+            // 
+            this.addFeed.Location = new System.Drawing.Point(149, 3);
+            this.addFeed.Name = "addFeed";
+            this.addFeed.Size = new System.Drawing.Size(25, 23);
+            this.addFeed.TabIndex = 0;
+            this.addFeed.Text = "+";
+            this.addFeed.UseVisualStyleBackColor = true;
+            this.addFeed.Click += new System.EventHandler(this.addFeed_Click);
+            // 
+            // removeFeed
+            // 
+            this.removeFeed.Location = new System.Drawing.Point(180, 3);
+            this.removeFeed.Name = "removeFeed";
+            this.removeFeed.Size = new System.Drawing.Size(26, 23);
+            this.removeFeed.TabIndex = 1;
+            this.removeFeed.Text = "-";
+            this.removeFeed.UseVisualStyleBackColor = true;
+            this.removeFeed.Click += new System.EventHandler(this.removeFeed_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 42);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Feeds";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelContent
+            // 
+            this.panelContent.BackColor = System.Drawing.Color.White;
+            this.panelContent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContent.Location = new System.Drawing.Point(224, 63);
+            this.panelContent.Name = "panelContent";
+            this.panelContent.Size = new System.Drawing.Size(511, 483);
+            this.panelContent.TabIndex = 2;
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.White;
+            this.panelMenu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMenu.Location = new System.Drawing.Point(224, 3);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(511, 54);
+            this.panelMenu.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -194,6 +285,9 @@
             this.tableLayoutPanel2.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -213,7 +307,14 @@
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutMapRSSToolStripMenuItem;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TreeView treeViewBase;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Button addFeed;
+        private System.Windows.Forms.Button removeFeed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelContent;
+        private System.Windows.Forms.Panel panelMenu;
     }
 }
 
--- MapRss/MapRss/Form1.cs
+++ MapRss/MapRss/Form1.cs
@@ -12,14 +12,35 @@ namespace MapRss
 {
     public partial class Form1 : Form
     {
+        TreeView m_treeViewTopics;
+        TreeView m_treeViewFeeds;
+
         public Form1()
         {
             InitializeComponent();
+
+
+            m_treeViewTopics = PopulateTopicView();
+            m_treeViewFeeds = PopulateFeedView();
+
+            foreach (TreeNode node in m_treeViewFeeds.Nodes)
+            {
+                treeViewBase.Nodes.Add((TreeNode)node.Clone());
+            }
+
+            treeViewBase.ExpandAll();
         }
 
         private void Feed_Click(object sender, EventArgs e)
         {
+            treeViewBase.Nodes.Clear();
 
+            foreach (TreeNode node in m_treeViewFeeds.Nodes)
+            {
+                treeViewBase.Nodes.Add((TreeNode)node.Clone());
+            }
+
+            treeViewBase.ExpandAll();
         }
 
         private void Map_Click(object sender, EventArgs e)
@@ -29,7 +50,14 @@ namespace MapRss
 
         private void Topic_Click(object sender, EventArgs e)
         {
+            treeViewBase.Nodes.Clear();
 
+            foreach (TreeNode node in m_treeViewTopics.Nodes)
+            {
+                treeViewBase.Nodes.Add((TreeNode)node.Clone());
+            }
+
+            treeViewBase.ExpandAll();
         }
 
         private void loadToolStripMenuItem_Click(object sender, EventArgs e)
@@ -50,6 +78,44 @@ namespace MapRss
         private void groupBox1_Enter(object sender, EventArgs e)
         {
 
+        }
+
+        private void addFeed_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void removeFeed_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //This will need to load from the XML save file to actually populate the topic area
+        private TreeView PopulateTopicView()
+        {
+            TreeView topics = new TreeView();
+
+            TreeNode[] topicAreas = new TreeNode[] { new TreeNode("Sports"), new TreeNode("Heath")};
+
+            TreeNode root = new TreeNode("Topics", topicAreas);
+
+            topics.Nodes.Add(root);
+
+            return topics;
+        }
+
+        //This will need to load from the XML save file to actually populate the topic area
+        private TreeView PopulateFeedView()
+        {
+            TreeView feed = new TreeView();
+
+            TreeNode[] topicAreas = new TreeNode[] { new TreeNode("NY Times"), new TreeNode("US News and World Report") };
+
+            TreeNode root = new TreeNode("Feed", topicAreas);
+
+            feed.Nodes.Add(root);
+
+            return feed;
         }
     }
 }
